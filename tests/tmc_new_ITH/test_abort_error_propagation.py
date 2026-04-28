ec160075fb04a867897361f3166c2f788bd93ef0
devesh.ska <devesh.ska@gmail.com>
2026-04-28 12:30:03 +0530

    HM-899: update exception

--- tests/tmc_new_ITH/test_abort_error_propagation.py
+++ tests/tmc_new_ITH/test_abort_error_propagation.py
@@ -32,8 +32,8 @@ LOGGER = logging.getLogger(__name__)
 
 COMMAND_RESULT_CSP = (
     '[3, "Exception occurred on the following devices: '
-    "mid-tmc/subarray-leaf-node-csp/01: "
-    'Exception occurred, command failed."]'
+    "mid-tmc/subarray-leaf-node-csp/01: Exception occurred on devices: "
+    'mid-csp/subarray/01: Exception occurred, command failed."]'
 )
 
 COMMAND_RESULT_SDP = (
